fe19e46b3eac5ff5c4c375fe583b2d4c786a0190
Nicolas Faessel <niko@localhost.localdomain>
2024-05-14 17:16:19 +0200

    [WIP] policy

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse; 
 use App\Models\Todo;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class TodoController extends Controller
 {
@@ -12,13 +13,22 @@ class TodoController extends Controller
 
     public function index(): JsonResponse
     {
+        Gate::authorize('viewAny', Todo::class);
         $user = Auth::user();
         $todos = $user->todos;
         return response()->json($todos); 
     }
 
+    public function show(Todo $todo) {
+        Gate::authorize('view', $todo);
+        return response()->json($todo);
+    }
+
     public function store(Request $request) : JsonResponse
     {
+        if($request->user()->cannot('create', Todo::class)) {
+            abort(403);
+        }
         $validated = $request->validate([
             "title" => ['required', 'string', 'max:255']
         ]);
@@ -27,5 +37,21 @@ class TodoController extends Controller
         $todo->user_id = Auth::user()->id;
         $todo->save(); 
         return response()->json($todo, 201);
+    }
+
+    public function update(Todo $todo): JsonResponse
+    {
+
+        $todo->completed = !$todo->completed;
+        $todo->save(); 
+        return response()->json($todo);
+    }
+
+    public function delete(Todo $todo) : JsonResponse
+    {
+        Gate::authorize('delete', $todo);
+        $deleted = $todo; 
+        $todo->delete(); 
+        return response()->json($deleted);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,12 @@ use App\Http\Controllers\TodoController;
 
 Route::post('/login', [AuthenticationController::class, 'login']);
 Route::get('/todos', [TodoController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/todos/{todo}', [TodoController::class, 'show'])->middleware('auth:sanctum');
 Route::post('/todos', [TodoController::class, 'store'])->middleware('auth:sanctum');
+Route::put('/todos/{todo}', [TodoController::class, 'update'])
+    ->middleware('can:update,todo')
+    ->middleware('auth:sanctum');
+Route::delete('/todos/{todo}', [TodoController::class, 'delete'])
+    ->can('delete', 'todo')
+    ->middleware('auth:sanctum');
+// Route::apiResource('/todos', TodoController::class)->middleware('auth:sanctum');
