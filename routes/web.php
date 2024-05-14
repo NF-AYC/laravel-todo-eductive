1f4dc37e7c32f1f662ef9bc58f3ced1ea841c61d
Nicolas Faessel <niko@localhost.localdomain>
2024-05-14 10:01:56 +0200

    moved login route to web

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/login', [AuthenticationController::class, 'login']);
 
 // require_once('api.php');
